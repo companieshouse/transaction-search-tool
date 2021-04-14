1da3d773ba0b3e463549d6bb78e04a3b3caf2026
EmrysRoberts <eroberts@companieshouse.gov.uk>
2021-04-14 15:10:45 +0100

    Force req and res typing

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -4,21 +4,19 @@ import { SignInInfoKeys } from "ch-node-session-handler/lib/session/keys/SignInI
 import { UserProfileKeys } from "ch-node-session-handler/lib/session/keys/UserProfileKeys";
 import config from "../config";
 import { createLogger } from "ch-structured-logging";
+import { ISignInInfo } from "ch-node-session-handler/lib/session/model/SessionInterfaces";
+import { NextFunction, Request, Response } from 'express';
 
 const logger = createLogger(config.applicationNamespace);
 
 const createAuthenticationMiddleware = function (): RequestHandler {
 
-    return (req, res, next) => {
+    return (req: Request, res: Response, next:NextFunction) => {
 
         logger.info(`Session is currently: ${req.session}`);
-        const sessionData = req.session['data'];
-        logger.info(`signed in info is currently: ${req.session[SessionKey.SignInInfo]}`);
-
-        const signInInfo = (sessionData !== undefined)? sessionData[SessionKey.SignInInfo] : undefined;
-        
-        if (signInInfo !== undefined) {
-
+        const signInInfo = getSignInInfo(req.session);
+        logger.info(`Sign in info is: ${signInInfo}`);
+        if (checkUserSignedIn(signInInfo)) {
             const signedIn = signInInfo[SignInInfoKeys.SignedIn] === 1;
             const userInfo = signInInfo[SignInInfoKeys.UserProfile];
 
@@ -26,15 +24,25 @@ const createAuthenticationMiddleware = function (): RequestHandler {
                 if (req.body === undefined) {
                     req.body = {};
                 }
-
                 req.body.loggedInUserEmail = userInfo[UserProfileKeys.Email];
-                logger.info(req.body.loggedInUserEmail);
+                logger.info(`logged in as: ${req.body.loggedInUserEmail}`);
                 return next();
             }
         }
 
         return res.redirect(`/signin?return_to=/${config.urlPrefix}/`);
     };
+
+    function checkUserSignedIn(signInInfo): boolean {
+        if (signInInfo !== undefined) {
+            return signInInfo[SignInInfoKeys.SignedIn] === 1;
+        }
+        return false;
+    }
+
+    function getSignInInfo(session): ISignInInfo {
+        return (session.data !== undefined) ? session.data[SessionKey.SignInInfo] : undefined;
+    }
 };
 
 export default createAuthenticationMiddleware;
