5c4e05e90775e0a13bd84d0ad2e47be9e33d7c9f
bcullerton <bcullerton@companieshouse.gov.uk>
2021-05-07 09:04:47 +0100

    Load local css in harmon1

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,7 @@ app.use(authenticationMiddleware());
 app.use(`/${config.urlPrefix}`, BarcodeSearchRouter.create());
 app.use(createLoggerMiddleware(config.applicationNamespace));
 app.use(`/${config.urlPrefix}/static`, express.static("dist/app/static"));
+env.addGlobal("CSS_URL", `/${config.urlPrefix}/static/app.css`);
 
 app.set('engine', env);
 
