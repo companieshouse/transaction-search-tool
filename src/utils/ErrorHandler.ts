f785deb2f02bb7eb60d342b83a9386ec15149c5f
EmrysRoberts <eroberts@companieshouse.gov.uk>
2021-07-29 12:47:33 +0100

    Add error page and render stack

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -7,6 +7,9 @@ class ErrorHandler {
     public handleError(className:string, functionName: string, err: Error, res?) {
         logger.error("Error in " + className + " function " + functionName+ ": " + err.message);
         res?.status(500);
+        res?.render("error", {
+            errorMessage: err.stack
+        });
     }
 }
 
