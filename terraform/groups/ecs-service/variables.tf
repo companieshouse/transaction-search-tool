5356602790916fe5807887c1172e3da38d4a6530
ivana-companieshouse <143526111+ivana-companieshouse@users.noreply.github.com>
2023-10-02 12:26:21 +0100

    Setting staging variables (#99)

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -28,19 +28,19 @@ variable "docker_registry" {
 # Service performance and scaling configs
 # ------------------------------------------------------------------------------
 variable "desired_task_count" {
-  type = number
+  type        = number
   description = "The desired ECS task count for this service"
-  default = 1 # defaulted low for dev environments, override for production
+  default     = 1 # defaulted low for dev environments, override for production
 }
 variable "required_cpus" {
-  type = number
+  type        = number
   description = "The required cpu resource for this service. 1024 here is 1 vCPU"
-  default = 128 # defaulted low for dev environments, override for production
+  default     = 128 # defaulted low for dev environments, override for production
 }
 variable "required_memory" {
-  type = number
+  type        = number
   description = "The required memory for this service"
-  default = 256 # defaulted low for node service in dev environments, override for production
+  default     = 256 # defaulted low for node service in dev environments, override for production
 }
 variable "use_fargate" {
   type        = bool
@@ -56,67 +56,31 @@ variable "log_level" {
   description = "The log level for services to use: trace, debug, info or error"
 }
 
-variable "chs_url" {
-  type        = string
-}
 variable "cdn_host" {
-  type        = string
+  type = string
 }
 variable "transaction_search_tool_web_version" {
   type        = string
   description = "The version of the transaction search tool web container to run."
 }
-variable "piwik_url" {
-  type        = string
-}
-variable "piwik_site_id" {
-  type        = string
-}
-variable "redirect_uri" {
-  type        = string
-  default     = "/"
-}
-variable "cache_pool_size" {
-  type        = string
-  default     = "8"
-}
+
 variable "cookie_domain" {
-  type        = string
+  type = string
 }
 variable "cookie_name" {
-  type        = string
-  default     = "__SID"
+  type    = string
+  default = "__SID"
 }
 variable "cookie_secure_only" {
-  type        = string
-  default     = "0"
-}
-variable "cookie_expiration_in_seconds" {
-  type        = string
-  default     = "3600"
+  type    = string
+  default = "0"
 }
 variable "default_session_expiration" {
-  type        = string
-  default     = "3600"
+  type    = string
+  default = "3600"
 }
-variable "download_filename_prefix" {
-  type        = string
-}
-variable "api_url" {
-  type        = string
-}
-variable "max_file_size_bytes" {
-  type        = string
-}
-variable "human_log" {
-  type        = string
-}
-
 variable "port" {
-  type = string
-  default = "18580" 
+  type    = string
+  default = "18580"
 }
 
-variable "show_service_offline_page" {
-  type        = string
-}
