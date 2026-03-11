752dcc1fcf92cfbb7f883b8f6cd49a396197f2d7
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2026-03-11 12:44:05 +0000

    ASM-1484: Align tsc outDir with docker-chs-development project directory mount to /app.

--- docker_start.sh
+++ docker_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start script for transaction-search-tool
 
-exec node --no-opt -r /proc/.reset --inspect=0.0.0.0:9229 -r ts-node/register dist/app/bin/nodemon-entry.js
+exec node --no-opt -r /proc/.reset --inspect=0.0.0.0:9229 -r ts-node/register dist/bin/nodemon-entry.js
