42361c09209697003f9b72432afd6619f757ad4e
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2026-03-24 12:36:45 +0000

    ASM-1484: SonarQube: Remove this useless assignment to variable "resultsMap".

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -19,39 +19,39 @@ class SearchController {
 
     public async searchQuery(req: any, res: any) {
         const searchTerm = req.query.search;
-        let resultsMap: Map<string,DocumentOverviewModel> = new Map();
 
         try {
-            resultsMap = await this.companyNumberSearchHandler.searchCompanyNumber(searchTerm);
+            let resultsMap = await this.companyNumberSearchHandler.searchCompanyNumber(searchTerm);
             const barcodeSearchResult = await this.barcodeSearchHandler.searchBarcode(searchTerm);
             if (!barcodeSearchResult.isEmpty()) resultsMap.set(searchTerm, barcodeSearchResult);
+
+            if (resultsMap.size === 0) {
+                res.render("search", {
+                    barcode: searchTerm,
+                    error: true
+                });
+            } else {
+                const models = this.getModelsAsArray(resultsMap);
+                if(resultsMap.size === 1) {
+                    const barcode = resultsMap.keys().next().value;
+                    const timelineModel = await this.barcodeSearchHandler.getTimelineResult(barcode, resultsMap.values().next().value);
+                    res.render("documentOverview", {
+                        barcode: barcode,
+                        result: models[0],
+                        timeline: timelineModel
+                    })
+                } else {
+                    res.render("resultsPage", {
+                        searchTerm: searchTerm,
+                        results: models
+                    });
+                }
+            }
         } catch(err) {
             errorHandler.handleError(this.constructor.name, "searchQuery", err, res);
             return;
         }
 
-        if (resultsMap.size === 0) {
-            res.render("search", {
-                barcode: searchTerm,
-                error: true
-            });
-        } else {
-            const models = this.getModelsAsArray(resultsMap);
-            if(resultsMap.size === 1) {
-                const barcode = resultsMap.keys().next().value;
-                const timelineModel = await this.barcodeSearchHandler.getTimelineResult(barcode, resultsMap.values().next().value);
-                res.render("documentOverview", {
-                    barcode: barcode,
-                    result: models[0],
-                    timeline: timelineModel
-                })
-            } else {
-                res.render("resultsPage", {
-                    searchTerm: searchTerm,
-                    results: models
-                });
-            }
-        }
     }
 
     private getModelsAsArray(resultsMap:Map<string,DocumentOverviewModel>): object[] {
